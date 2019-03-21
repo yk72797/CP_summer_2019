c1ef2dfeb2e96c4d4b872f634ba4129ea07fb888
prubac <prubac@sgh.waw.pl>
2019-03-21 11:05:27 +0100

    Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,34 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        int[] intArr = new int[5];
+        //Integer[] intArr = new Integer[5];
+        intArr[0] = 567;
+        intArr[1] = 968;
+        intArr[2] = 675;
+
+        String[] strArr = new String[] {
+                "ABC",
+                "DEF",
+                "EFG"
+        };
+
+        for (int i=0;i<intArr.length;i++) {
+            //if (intArr[i]!=null)
+                System.out.println(i + "th element: " + intArr[i]*10);
+        }
+
+        for (int myEl : intArr) {
+            System.out.println(myEl);
+        }
+
+        // For Each loop
+        for (String el : strArr) {
+            System.out.println("My strArr element: " + el);
+        }
+
+
+
+
+    }
+}
